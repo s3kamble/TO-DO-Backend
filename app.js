b2b77292f8c2be04615b0bf8e67d74f11f793aea
Shreya <shreyak438@gmail.com>
2021-08-15 18:58:32 +0530

    Heroku port changes

--- app.js
+++ app.js
@@ -2,18 +2,25 @@ const express = require("express");
 const taskRouter = require("./routes/taskRouter");
 const app = express();
 
+// const path=require("path")
+// const dotenv = require("dotenv")
+// dotenv.config({path: "./config.env"})
+
+
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Headers","Access-Control-Allow-Headers ,Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method")
     next();
 });
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
-
 app.use("/tasks",taskRouter);
 
 
 
-app.listen(3000,()=>{
-    console.log("Server started at port 3000");
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
