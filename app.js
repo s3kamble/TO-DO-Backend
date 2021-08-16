035580089a5c11506f2ca34d43421036b71f5fd2
Shreya <shreyak438@gmail.com>
2021-08-16 14:11:58 +0530

    Write markdown

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-// let text= `<embed type="text/markdown" src="https://s3kamble.github.io/TO-DO-Backend/" height="100%" width="100%"/>`;
+let text= `<embed type="text/markdown" src="https://s3kamble.github.io/TO-DO-Backend/" height="100%" width="100%"/>`;
 
-// fs.writeFileSync("./public/index.html", text);
-// app.use(express.static("public"));
+fs.writeFileSync("./public/index.html", text);
+app.use(express.static("public"));
 
 
 app.use((req, res, next) => {
