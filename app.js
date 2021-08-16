63d60267e270582ec5cd776494f2ad11b7ffff38
Shreya <shreyak438@gmail.com>
2021-08-16 14:07:03 +0530

    Express error solved

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const fs = require("fs");   
 const express = require("express");
-const taskRouter = require("./routes/taskRouter");
+const taskRouter = require("./routes/taskRouter.js");
 const app = express();
 
 // const path=require("path")
 // const dotenv = require("dotenv")
 // dotenv.config({path: "./config.env"})
 
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+// let text= `<embed type="text/markdown" src="https://s3kamble.github.io/TO-DO-Backend/" height="100%" width="100%"/>`;
 
-let text= `<embed type="text/markdown" src="https://s3kamble.github.io/TO-DO-Backend/" height="100%" width="100%"/>`;
-
-fs.writeFileSync("./public/index.html", text);
-app.use(express.static("public"))
+// fs.writeFileSync("./public/index.html", text);
+// app.use(express.static("public"));
 
 
 app.use((req, res, next) => {
@@ -20,8 +21,8 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Headers","Access-Control-Allow-Headers ,Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method")
     next();
 });
-app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+
+
 
 app.use("/tasks",taskRouter);
 
